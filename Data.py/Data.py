58a7dc3efdaaa11ac51496a8dbbe05440e9ecbfc
Chaiwat <c.rukkusol@gmail.com>
2020-12-22 22:02:07 +0700

    New

--- Data.py/Data.py
+++ Data.py/Data.py
@@ -1,6 +1,2 @@
 import pandas as pd
 import numpy as np
-
-df  = pd.read_html('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M')[0]
-df = df[df['Borough'] != 'Not assigned'].reset_index(drop=True)
-df.head(15)
